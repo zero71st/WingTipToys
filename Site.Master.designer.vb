e6f850d0537de2f4644b4be0646ba4e2af6b8d96
MisX7 <zero71st@gmail.com>
2013-08-16 22:14:44 +0700

    Added category list view to site.master

--- Site.Master.designer.vb
+++ Site.Master.designer.vb
@@ -41,6 +41,15 @@ Partial Public Class SiteMaster
     Protected WithEvents Logo As Global.System.Web.UI.WebControls.Image
 
     '''<summary>
+    '''categorylist control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents categorylist As Global.System.Web.UI.WebControls.ListView
+
+    '''<summary>
     '''FeaturedContent control.
     '''</summary>
     '''<remarks>
