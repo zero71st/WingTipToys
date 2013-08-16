d028de9c2bd1e1b9ebc17a347b229dbc3900b7cb
MisX7 <zero71st@gmail.com>
2013-08-16 21:20:06 +0700

    Added seed data.

--- Models/ProductDatabaseInitializer.vb
+++ Models/ProductDatabaseInitializer.vb
@@ -4,4 +4,33 @@ Public Class ProductDatabaseInitializer : Inherits DropCreateDatabaseIfModelChan
         MyBase.Seed(context)
     End Sub
 
+    Private Shared Function GetCategories() As List(Of Category)
+        Dim categories = New List(Of Category)
+        categories.Add(New Category With {.CategoryID = 1, .CategoryName = "Cars"})
+        categories.Add(New Category With {.CategoryID = 2, .CategoryName = "Planes"})
+        categories.Add(New Category With {.CategoryID = 3, .CategoryName = "Trucks"})
+        categories.Add(New Category With {.CategoryID = 4, .CategoryName = "Boats"})
+        categories.Add(New Category With {.CategoryID = 5, .CategoryName = "Rockets"})
+
+        Return categories
+    End Function
+
+    Private Shared Function GetProducts() As List(Of Product)
+        Dim products = New List(Of Product)
+        products.Add(New Product With {.ProductID = 1,
+                                        .ProductName = "Convertible Car",
+                    .Description = "This convertible car is fast! The engine is powered by a neutrino based battery (not included)." +
+                                  "Power it up and let it go!",
+                    .ImagePath = "carconvert.png",
+                    .UnitPrice = 22.5,
+                    .CategoryID = 1})
+        products.Add(New Product With {.ProductID = 2,
+                                        .ProductName = "Old-time Car",
+                    .Description = "There's nothing old about this toy car, except it's looks.",
+                    .ImagePath = "carearly.png",
+                    .UnitPrice = 19.5,
+                    .CategoryID = 1})
+        Return products
+    End Function
+
 End Class
